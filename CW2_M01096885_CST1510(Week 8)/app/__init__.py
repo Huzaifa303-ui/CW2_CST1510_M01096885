ba505620575e468898f2ed80c1273f91f815dd2b
Huzaifa Ahmad <HA1399@live.mdx.ac.uk>
2025-11-26 10:21:46 +0400

    Delete CW2_M01096885_CST1510(Week 8) directory

--- CW2_M01096885_CST1510(Week 8)/app/__init__.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-
-Path("C:/Users/User/VS code 1/CW2_M01096885_CST1510(Week 8)/app/__init__.py").touch()
-Path("C:/Users/User/VS code 1/CW2_M01096885_CST1510(Week 8)/app/data/__init__.py").touch()
-Path("C:/Users/User/VS code 1/CW2_M01096885_CST1510(Week 8)/app/services/__init__.py").touch()
